96640a47391552d00ad633dbe508430fac47b681
laurensdehoorne <55842703+laurensdehoorne@users.noreply.github.com>
2025-06-15 20:37:26 +0200

    Update __init__.py Fix for HA 2025.6 update

--- custom_components/alsavopro/__init__.py
+++ custom_components/alsavopro/__init__.py
@@ -43,10 +43,7 @@ async def async_setup_entry(hass, entry):
         hass.data[DOMAIN] = {}
     hass.data[DOMAIN][entry.entry_id] = data_coordinator
 
-    for platform in ('sensor', 'climate'):
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, ['sensor', 'climate'])
 
     return True
 
